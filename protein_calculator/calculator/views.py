d41c488a2a8289f07e53730030f2ec09931178a1
Chii Law <cyee3012@hotmail.co.uk>
2022-05-16 16:53:06 +0900

    added and changed code so html can be inherited from html files

--- protein_calculator/calculator/views.py
+++ protein_calculator/calculator/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
 
 def home(request):
-  return HttpResponse("<h1>Calculator Homepage</h1>")
+  return render(request, 'calculator/home.html')
 
 def about(request):
-  return HttpResponse("<h1>About</h1>"
-    "<h2>What is Protein calculator?</h2>"
-    "<h3>Protein calculator was created to easily calculate protein intake, to help those who are trying to gain muscle 💪🏼</h3>"
-    "<p>Site created with Django and Python 🐍"
-  )
+  return render(request, 'calculator/about.html', {'title': 'About'})
