9ce7d96c4be7ef0c706d3ae735c93248e9f061c9
Chii Law <cyee3012@hotmail.co.uk>
2022-05-17 14:19:30 +0900

    created form that can take input for kgs/lbs and weight and redirects to results page.

--- protein_calculator_site/calculator/views.py
+++ protein_calculator_site/calculator/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from .forms import ProteinForm
 
 def home(request):
-  return render(request, 'calculator/home.html')
+  if request.method == "POST":
+    form = ProteinForm(request.POST)
+
+    if form.is_valid():
+      form.save()
+      system = form.cleaned_data['system']
+      weight = form.cleaned_data['weight']
+      return render(request, 'calculator/results.html', {'title': 'Results', 'weight': weight})
+  else:
+    form = ProteinForm()
+
+  form = ProteinForm()
+  return render(request, 'calculator/home.html', {'form': form})
 
 def about(request):
   return render(request, 'calculator/about.html', {'title': 'About'})
 
 def results(request):
-  return render(request, 'calculator/results.html', {'title': 'Results'})
+  return render(request, 'calculator/results.html', {'title': 'Results', 'weight': 'weight'})
