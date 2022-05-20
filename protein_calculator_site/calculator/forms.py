5aa8a8e2bc87b7fa2b838430c13e73ea2b24814b
Chii Law <cyee3012@hotmail.co.uk>
2022-05-20 18:06:22 +0900

    Simple styling applied to look more neat

--- protein_calculator_site/calculator/forms.py
+++ protein_calculator_site/calculator/forms.py
@@ -3,5 +3,5 @@ from django import forms
 SYSTEM_CHOICES = [("metric", "kgs"), ("imperial","lbs")]
 
 class ProteinForm(forms.Form):
-  system = forms.CharField(label="kgs or lbs?", widget=forms.Select(choices=SYSTEM_CHOICES))
+  system = forms.CharField(label="Metric or Imperial?", widget=forms.Select(choices=SYSTEM_CHOICES))
   weight = forms.CharField(label="Weight", max_length=3)
