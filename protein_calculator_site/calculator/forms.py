9c2e2454272e5bd4e7303f87f63028dd87661330
Chii Law <cyee3012@hotmail.co.uk>
2022-05-23 16:53:45 +0900

    added crispy form to style form, changed icon and favicon

--- protein_calculator_site/calculator/forms.py
+++ protein_calculator_site/calculator/forms.py
@@ -1,7 +1,17 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
-SYSTEM_CHOICES = [("metric", "kgs"), ("imperial","lbs")]
 
 class ProteinForm(forms.Form):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.helper = FormHelper(self)
+    self.helper.form_method = 'POST'
+    self.helper.add_input(Submit('submit', 'calculate!'))
+
+  SYSTEM_CHOICES = [("metric", "kgs"), ("imperial","lbs")]
+
   system = forms.CharField(label="Metric or Imperial?", widget=forms.Select(choices=SYSTEM_CHOICES))
-  weight = forms.CharField(label="Weight", max_length=3)
+  weight = forms.IntegerField()
