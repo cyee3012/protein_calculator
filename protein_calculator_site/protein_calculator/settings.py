27f895acf8bbbbe40f245cf30e297c18d8e8abe5
Chii Law <cyee3012@hotmail.co.uk>
2022-05-24 19:42:12 +0900

    modified settinngs.py according to heroku's article on djanjo assets

--- protein_calculator_site/protein_calculator/settings.py
+++ protein_calculator_site/protein_calculator/settings.py
@@ -12,8 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+## Build paths inside the project like this: BASE_DIR / 'subdir'.
+## original from when app was generated
+## BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# suggested by heroku for deploying django apps
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -114,10 +120,21 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+## Static files (CSS, JavaScript, Images)
+## https://docs.djangoproject.com/en/4.0/howto/static-files/
+## originally included when app generated
+## STATIC_URL = 'static/'
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# suggested by heroku for deploying django apps
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
